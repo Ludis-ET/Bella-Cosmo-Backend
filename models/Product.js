58ef97a7d33a0087e606e9a01fac7f22600510cd
Ludis <leulsegedmelaku1020@gmail.com>
2024-09-29 11:57:29 +0300

    Refactor Product model to support multiple image URLs

--- models/Product.js
+++ models/Product.js
@@ -4,7 +4,6 @@ const productSchema = new mongoose.Schema({
   price: { type: Number, required: true },
   category: { type: String, required: true },
   stock: { type: Number, default: 0 },
-  image: { type: String }, // for primary image
   image1Url: { type: String },
   image2Url: { type: String },
   image3Url: { type: String },
